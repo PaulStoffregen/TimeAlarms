271cf4585fcf054c149965434cac73363c645e29
PaulStoffregen <paul@pjrc.com>
2016-07-06 08:14:10 -0700

    Update example again - better demonstration of Alarm.free()

--- examples/TimeAlarmExample/TimeAlarmExample.ino
+++ examples/TimeAlarmExample/TimeAlarmExample.ino
@@ -11,17 +11,15 @@
  * At startup the time is set to Jan 1 2011  8:29 am
  */
 
-// Questions?  Ask them here:
-// http://forum.arduino.cc/index.php?topic=37693.0
-
 #include <TimeLib.h>
 #include <TimeAlarms.h>
 
-AlarmId onceId;
+AlarmId id;
 
-void setup()
-{
+void setup() {
   Serial.begin(9600);
+  while (!Serial) ; // wait for Arduino Serial Monitor
+
   setTime(8,29,0,1,1,11); // set time to Saturday 8:29:00am Jan 1 2011
 
   // create the alarms, to trigger at specific times
@@ -31,45 +29,51 @@ void setup()
 
   // create timers, to trigger relative to when they're created
   Alarm.timerRepeat(15, Repeats);           // timer for every 15 seconds
-  onceId = Alarm.timerOnce(10, OnceOnly);   // called once after 10 seconds
+  id = Alarm.timerRepeat(2, Repeats2);      // timer for every 2 seconds
+  Alarm.timerOnce(10, OnceOnly);            // called once after 10 seconds
 }
 
-void  loop() {
+void loop() {
   digitalClockDisplay();
   Alarm.delay(1000); // wait one second between clock display
 }
 
 // functions to be called when an alarm triggers:
-void MorningAlarm(){
+void MorningAlarm() {
   Serial.println("Alarm: - turn lights off");
 }
 
-void EveningAlarm(){
+void EveningAlarm() {
   Serial.println("Alarm: - turn lights on");
 }
 
-void WeeklyAlarm(){
+void WeeklyAlarm() {
   Serial.println("Alarm: - its Monday Morning");
 }
 
-void ExplicitAlarm(){
+void ExplicitAlarm() {
   Serial.println("Alarm: - this triggers only at the given date and time");
 }
 
-void Repeats(){
+void Repeats() {
   Serial.println("15 second timer");
 }
 
-void OnceOnly(){
-  Serial.println("This timer only triggers once");
-  // use Alarm.free() to recycle the memory used by an alarm
-  // after it is no longer needed.
-  Alarm.free(onceId);
-  onceId = dtINVALID_ALARM_ID; // safest to "forget" the ID after recycled
+void Repeats2() {
+  Serial.println("2 second timer");
 }
 
-void digitalClockDisplay()
-{
+void OnceOnly() {
+  Serial.println("This timer only triggers once, stop the 2 second timer");
+  // use Alarm.free() to disable a timer and recycle its memory.
+  Alarm.free(id);
+  // optional, but safest to "forget" the ID after memory recycled
+  id = dtINVALID_ALARM_ID;
+  // you can also use Alarm.disable() to turn the timer off, but keep
+  // it in memory, to turn back on later with Alarm.enable().
+}
+
+void digitalClockDisplay() {
   // digital clock display of the time
   Serial.print(hour());
   printDigits(minute());
@@ -77,8 +81,7 @@ void digitalClockDisplay()
   Serial.println();
 }
 
-void printDigits(int digits)
-{
+void printDigits(int digits) {
   Serial.print(":");
   if (digits < 10)
     Serial.print('0');
